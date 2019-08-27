51203caf82e81c54e3347c44eaac2df652d38e69
Myr <myr@MacBook-Pro-de-Myr.local>
2019-08-27 14:57:41 +0200

    alpha 1.0.9

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ const data = require('./lib/data')
 
 const renderOpts = {
   dashColor: 'cyan',
-  keysColor: 'blue',
+  keysColor: 'magenta',
   stringColor: 'white'
 }
 
 module.exports = ({ json }) => json ? JSON.stringify(data) : render(data, renderOpts)
+
+
+var figlet = require('figlet');
+
+figlet('Hello World!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
